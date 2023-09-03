edfa84bcd6e2585b3d58eed6e99f8eab60f92469
mironovisa <mironovisa@gmail.com>
2023-09-03 05:22:00 +0300

    Deleted Mongo Con

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require("cors");
 const { verify } = require("./utils/jwt");
 require("dotenv").config();
-const runMongoDB = require("./utils/mongo");
 
 app.use(
   cors({
@@ -15,29 +14,14 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.set("Content-Type", "text/html");
+
   res.send("hey bro");
 });
 
 app.use("/auth", require("./routes/auth.route"));
 
-async function checkMongoDBConnection() {
-  const databaseName = "NFTMarketPlace";
-  const collectionName = "users";
+const port = process.env.PORT || 3002;
 
-  try {
-    const collection = await runMongoDB(databaseName, collectionName);
-
-    console.log("MongoDB connection is OK");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
-  }
-}
-
-checkMongoDBConnection().then(() => {
-  const port = process.env.PORT || 3002;
-
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
